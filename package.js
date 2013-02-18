a307c055deb983c6211efacee67904b2e640bea0
Rai Phairow <rai.phairow@gmail.com>
2013-02-18 12:27:29 -0800

    create package

---
+++ package.js
@@ -0,0 +1,14 @@
+{
+	"name": "sender"
+	"version": "0.0.1",
+	"description": "browser utility",
+	"author": "Rai Phairow <rai.phairow@gmail.com>,
+	"dependencies": {
+		"bean": "1.0.3",
+		"bonzo": "1.3.4",
+		"domready": "0.2.11",
+		"qwery": "3.4.1",
+		"q": "0.8.12"
+	},
+	"main": "sender.js"
+}
